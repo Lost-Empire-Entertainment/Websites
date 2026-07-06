282463e7a3e21bbb83637c32c7c6f8740c83cc4b
greeenlaser <sanderveski@gmail.com>
2026-07-06 10:58:02 +0300

    proper init and shutdown

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -6,6 +6,8 @@
 #ifdef _WIN32
 #include <windows.h>
 #include <shlobj.h>
+#include <consoleapi.h>
+#include <processthreadsapi.h>
 #else
 #include <signal.h>
 #endif
@@ -37,137 +39,134 @@ using std::this_thread::sleep_for;
 using std::chrono::milliseconds;
 using std::string;
 
-static bool running = true;
-
 #ifdef _WIN32
 BOOL WINAPI HandleClose(DWORD signal)
 {
     if (signal == CTRL_C_EVENT
-        || signal == CTRL_CLOSE_EVENT)
+        || signal == CTRL_CLOSE_EVENT
+        || signal == CTRL_BREAK_EVENT)
     {
-        running = false;
+        KalaServerCore::Shutdown();
+        ExitProcess(0);
+
+        return TRUE;
     }
     
-    return TRUE;
+    return FALSE;
 }
 #else
-static void HandleClose(int) { running.store(false); }
+static void HandleClose(int)
+{ 
+    KalaServerCore::Shutdown();
+    _exit(0);
+}
 #endif
 
 int main()
 {
- #ifdef _WIN32
-        SetConsoleCtrlHandler(HandleClose, TRUE);
+#ifdef _WIN32
+    SetConsoleCtrlHandler(HandleClose, TRUE);
 #else
-        signal(SIGINT, HandleClose);
-        signal(SIGTERM, HandleClose);
+    signal(SIGINT, HandleClose);
+    signal(SIGTERM, HandleClose);
 #endif
 
-        path content = weakly_canonical(current_path() / "content");
+    path content = weakly_canonical(current_path() / "content");
 
-        if (!exists(content))
+    if (!exists(content))
+    {
+        Log::Print(
+            "Failed to find content folder!",
+            "WEBSITE_BACKEND",
+            LogType::LOG_ERROR,
+            2);
+
+        exit(1);
+    }
+
+    Log::Print(
+        "Found content folder '" + content.string() + "'",
+        "WEBSITE_BACKEND",
+        LogType::LOG_INFO);
+
+    KalaServerCore::Initialize(
+        "website_backend",
+        content,
+        "192.168.1.102",
+        80,
+        false);
+
+    /*
+    KalaServerCore::AddRoute( 
         {
-            Log::Print(
-                "Failed to find content folder!",
-                "WEBSITE_BACKEND",
-                LogType::LOG_ERROR,
-                2);
+            .domain = "thekalakit.com",
+            .route = "/",
+            .routePath = "."  
+        });
 
+    KalaServerCore::AddRoute( 
+        {
+            .domain = "elypsoengine.com",
+            .route = "/112233",
+            .routePath = "."  
+        });
+
+    KalaServerCore::AddBlacklistedKeyword("/wp-");
+    KalaServerCore::AddBlacklistedKeyword("/user");
+    KalaServerCore::AddBlacklistedKeyword("/login");
+    KalaServerCore::AddBlacklistedKeyword("/admin");
+    KalaServerCore::AddBlacklistedKeyword(".php");
+    KalaServerCore::AddBlacklistedKeyword(".env");
+    KalaServerCore::AddBlacklistedKeyword(".git");
+    KalaServerCore::AddBlacklistedKeyword(".json");
+    KalaServerCore::AddBlacklistedKeyword(".sql");
+    KalaServerCore::AddBlacklistedKeyword(".sh");
+    KalaServerCore::AddBlacklistedKeyword("bin");
+    */
+
+    if (KalaServerCore::IsCloudflareRequired())
+    {
+#ifdef _WIN32
+        PWSTR winUserDir{};
+        SHGetKnownFolderPath(FOLDERID_Profile, 0, NULL, &winUserDir);
+
+        path userDir = path(winUserDir) / ".cloudflared";
+        CoTaskMemFree(winUserDir);
+
+        path cloudflareExePath = current_path() / "cloudflared-windows-amd64.exe";
+#else
+        path userDir = path(getenv("HOME")) / ".cloudflared";
+        path cloudflareExePath = current_path() / "cloudflared-linux-amd64";
+#endif
+
+        if (!Cloudflare::Initialize(
+            "website_backend_tunnel", 
+            cloudflareExePath, 
+            userDir))
+        {
             exit(1);
         }
 
-        Log::Print(
-            "Found content folder '" + content.string() + "'",
-            "WEBSITE_BACKEND",
-            LogType::LOG_INFO);
-
-        KalaServerCore::Initialize(
-            "website_backend",
-            content,
-            { "thekalakit.com", "elypsoengine.com" },
-            "192.168.1.102",
-            80,
-            false);
-
-        /*
-        KalaServerCore::AddRoute( 
-            {
-                .domain = "thekalakit.com",
-                .route = "/",
-                .routePath = "."  
-            });
-
-        KalaServerCore::AddRoute( 
-            {
-                .domain = "elypsoengine.com",
-                .route = "/112233",
-                .routePath = "."  
-            });
-
-        KalaServerCore::AddBlacklistedKeyword("/wp-");
-        KalaServerCore::AddBlacklistedKeyword("/user");
-        KalaServerCore::AddBlacklistedKeyword("/login");
-        KalaServerCore::AddBlacklistedKeyword("/admin");
-        KalaServerCore::AddBlacklistedKeyword(".php");
-        KalaServerCore::AddBlacklistedKeyword(".env");
-        KalaServerCore::AddBlacklistedKeyword(".git");
-        KalaServerCore::AddBlacklistedKeyword(".json");
-        KalaServerCore::AddBlacklistedKeyword(".sql");
-        KalaServerCore::AddBlacklistedKeyword(".sh");
-        KalaServerCore::AddBlacklistedKeyword("bin");
-        */
-
-        if (KalaServerCore::IsCloudflareRequired())
+        while (!KalaServerCore::IsReady())
         {
-#ifdef _WIN32
-            PWSTR winUserDir{};
-            SHGetKnownFolderPath(FOLDERID_Profile, 0, NULL, &winUserDir);
+            sleep_for(milliseconds(1000));
 
-            path userDir = path(winUserDir) / ".cloudflared";
-            CoTaskMemFree(winUserDir);
-
-            path cloudflareExePath = current_path() / "cloudflared-windows-amd64.exe";
-#else
-            path userDir = path(getenv("HOME")) / ".cloudflared";
-            path cloudflareExePath = current_path() / "cloudflared-linux-amd64";
-#endif
-
-            if (!Cloudflare::Initialize(
-                "website_backend_tunnel", 
-                cloudflareExePath, 
-                userDir))
-            {
-                exit(1);
-            }
-
-            while (!KalaServerCore::IsReady())
-            {
-                sleep_for(milliseconds(1000));
-
-                Log::Print(
-                    "Waiting for Cloudflare to finish connecting...",
-                    "WEBSITE_BACKEND",
-                    LogType::LOG_INFO);
-            }
+            Log::Print(
+                "Waiting for Cloudflare to finish connecting...",
+                "WEBSITE_BACKEND",
+                LogType::LOG_INFO);
         }
+    }
 
-        Log::Print(
-            "Website backend is ready, starting accept loop!",
-            "WEBSITE_BACKEND",
-            LogType::LOG_INFO);
+    Log::Print(
+        "Website backend is ready, starting accept loop!",
+        "WEBSITE_BACKEND",
+        LogType::LOG_INFO);
 
-        //mandatory loop to keep server alive
-        while (running)
-        { 
-            KalaServerCore::Update();
+    while (true)
+    { 
+        KalaServerCore::Update();
 
-            sleep_for(milliseconds(10));
-        }
-
-        Log::Print(
-            "Website backend is shutting down.",
-            "WEBSITE_BACKEND",
-            LogType::LOG_INFO);
-
-        KalaServerCore::Shutdown();   
+        sleep_for(milliseconds(10));
+    }
 }
